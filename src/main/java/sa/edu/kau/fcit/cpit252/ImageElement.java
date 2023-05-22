45b6a9bb18e78b014e3071ac052592599631c8aa
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-05-22 20:51:59 +0300

    init commit

---
+++ src/main/java/sa/edu/kau/fcit/cpit252/ImageElement.java
@@ -0,0 +1,27 @@
+package sa.edu.kau.fcit.cpit252;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
+import javax.swing.ImageIcon;
+
+public class ImageElement {
+
+    private String name;
+    public ImageElement(String name) {
+        this.name = name;
+    }
+
+    public String myPosition(int position) {
+        return "" + position + " " + this.name + ": " + this;
+    }
+
+    public ImageIcon getImageElement() {
+        try {
+            return new ImageIcon(Paths.get("images", this.name).toString());
+        }
+        catch(InvalidPathException e){
+            System.err.println("Invalid Path");
+        }
+        return null;
+    }
+}

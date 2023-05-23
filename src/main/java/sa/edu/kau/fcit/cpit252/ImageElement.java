69687ef80abc4f84e7a4ffd658c4fd91498c4914
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-05-23 06:21:20 +0300

    use maven resources directory

--- src/main/java/sa/edu/kau/fcit/cpit252/ImageElement.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/ImageElement.java
@@ -1,12 +1,16 @@
 package sa.edu.kau.fcit.cpit252;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Paths;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 
 public class ImageElement {
 
     private String name;
+
     public ImageElement(String name) {
         this.name = name;
     }
@@ -15,13 +19,12 @@ public class ImageElement {
         return "" + position + " " + this.name + ": " + this;
     }
 
-    public ImageIcon getImageElement() {
-        try {
-            return new ImageIcon(Paths.get("images", this.name).toString());
+    public ImageIcon getImageElement() throws FileNotFoundException {
+        URL imageURL = this.getClass().getResource(File.separator + "images" + File.separator + this.name);
+        ImageIcon imageIcon = new ImageIcon(imageURL);
+        if (imageIcon.getImageLoadStatus() != MediaTracker.COMPLETE) {
+            throw new FileNotFoundException(String.format("Failed to load image file %s", imageURL.getFile()));
         }
-        catch(InvalidPathException e){
-            System.err.println("Invalid Path");
-        }
-        return null;
+        return imageIcon;
     }
 }
